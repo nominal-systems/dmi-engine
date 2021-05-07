4a90ed9022eca42aaef818e370f6f0e9b131e808
Miguel Araujo <miguel@linehq.com>
2021-05-07 11:57:34 -0400

    Add missing queue registration in Idexx Module

--- src/providers/idexx/idexx.module.ts
+++ src/providers/idexx/idexx.module.ts
@@ -1,9 +1,16 @@
+import { BullModule } from '@nestjs/bull'
 import { HttpModule, Module } from '@nestjs/common'
+import { Provider } from '../../common/interfaces/provider-integration'
 import { IdexxController } from './idexx.controller'
 import { IdexxService } from './idexx.service'
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule,
+    BullModule.registerQueue({
+      name: `${Provider.Idexx}.results`
+    })
+  ],
   controllers: [IdexxController],
   providers: [IdexxService]
 })
